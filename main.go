77fbde95e5f72dd0882b14f860e6e81ec08c48de
kaleb.dueck@gmail.com <kaleb.dueck@gmail.com>
2020-11-18 21:29:29 -0800

    cracking 1.1

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("nothing to see here")
+}
+
+func isUnique(s string) bool {
+	charMap := make(map[rune]bool)
+
+	for _, char := range s {
+		_, exists := charMap[char]
+		if exists == true {
+			return false
+		}
+
+		charMap[char] = true
+	}
+
+	return true
+}
+
+func isUniqueSavingSpace(s string) bool {
+	for i, char := range s {
+		//no need to look at chars we've already looked at
+		for j := i + 1; j < len(s); j++ {
+			if char == []rune(s)[j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
---
+++ main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import "testing"
+
+func Test_isUnique(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "simple unique string returns true",
+			args: args{s: "hiya bud"},
+			want: true,
+		},
+		{
+			name: "simple non-unique string returns false",
+			args: args{s: "hiya buddy"},
+			want: false,
+		},
+		{
+			name: "zero length string is unique i guess",
+			args: args{s: ""},
+			want: true,
+		},
+		{
+			name: "one char length string is unique",
+			args: args{s: "a"},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isUnique(tt.args.s); got != tt.want {
+				t.Errorf("isUnique() = %v, want %v", got, tt.want)
+			}
+
+			if got := isUniqueSavingSpace(tt.args.s); got != tt.want {
+				t.Errorf("isUnique() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
